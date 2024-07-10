7cf2d2a18ba4895da50c0903f5a9611a4bbdfc3b
Georgii L. <chillmine85@gmail.com>
2024-07-10 19:50:14 +0300

    yyhyyyyyyyxxxxxxxx Artemr

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace project;
+class Program
+{
+     public static void Main()
+     {
+        System.Console.WriteLine("Hello");
+     }
+}
