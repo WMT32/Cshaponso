05d9217259ab886c2338e5be73aa93e683e449af
Georgii L. <chillmine85@gmail.com>
2024-07-10 20:43:46 +0300

    Added readme

--- Program.cs
+++ Program.cs
@@ -5,5 +5,6 @@ class Program
      public static void Main()
      {
         System.Console.WriteLine("Hello");
+        
      }
 }
---
+++ README.md
@@ -0,0 +1,6 @@
+# TEst test man
+
+## C3
+
+- Learnt Git
+- Gained knoweledge of working with _Git_
